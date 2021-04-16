4b98b33a4bc0d2e8655647fa75c087729885a8a4
Robert Esquivel <roberte1311@gmail.com>
2021-04-16 13:00:59 -0500

    Complete test for Target.com iphone search where title description and price of each result are persisted to text file.

--- src/main/java/com/techbee/seleniumtests/TargetTest.java
+++ src/main/java/com/techbee/seleniumtests/TargetTest.java
@@ -21,15 +21,19 @@ public class TargetTest {
 	private static PrintWriter out; 
 	
 	public static void main(String[] args) throws InterruptedException {
+		/** Setting up Driver for specific web-site.
+		 */
 		try {
 			out = new PrintWriter(new FileWriter(TARGETIPHONES));
 		} catch (IOException e) {
 			System.out.println("Could not write to file");
 		}
 
+		//setting Chrome driver property and its file path
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\rober\\Documents\\WebDrivers\\chromedriver_win32-89\\chromedriver.exe");
-
+		
+		//Instantiating Chrome driver object for eBay
 		WebDriver targetDriver = new ChromeDriver();
 		targetDriver.get("https://www.target.com/");
 		targetDriver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS) ;
@@ -38,54 +42,72 @@ public class TargetTest {
 
 	}
 	
+	/** Runs a search by entering input and submitting to view result and invoke 
+	 * action on them. */
 	public static void findTargetIphoneDetailsAndSaveList(WebDriver targetDriver) throws InterruptedException {
 		WebDriverWait wait=new WebDriverWait(targetDriver, 20);
+		
+		//entering input
 		targetDriver.findElement(By.id("search")).sendKeys("iphone");
 		Thread.sleep(1000);
+		
+		//submitting input
 		targetDriver.findElement(By.id("search")).submit();
 		Thread.sleep(5000);
+		
+		//Collecting specified Web Elements by CSS Selector
 		List<WebElement> items = wait.until(ExpectedConditions
 				.visibilityOfAllElementsLocatedBy(
 				By.cssSelector(
 						"[data-test = list-entry-product-card]")));
-		//targetDriver.findElements(By.cssSelector("[data-test = list-entry-product-card]"));
 		Thread.sleep(1000);
+		
+		//Scrolls down to bottom of page so page will be fully loaded
 		JavascriptExecutor jse = (JavascriptExecutor) targetDriver;
 		jse.executeScript("window.scrollTo(0,document.body.scrollHeight);");
 
-		//System.out.println(items.size());
 		findIphoneDescriptionAndPrice(items, targetDriver);
 		
 	}
 	
-	public static void findIphoneDescriptionAndPrice(List<WebElement> items, WebDriver targetDriver) throws InterruptedException{
+	/** Finds title description and price of given list of Web Elements*/
+	public static void findIphoneDescriptionAndPrice(List<WebElement> items, 
+			WebDriver targetDriver) throws InterruptedException{
+		
+		/* Declaring WebdriverWait class to wait if the driver is taking some 
+		time to find specified elements based on specified locator */
 		WebDriverWait wait=new WebDriverWait(targetDriver, 20);
-		//System.out.println("loop method");
-		System.out.println(items.size());
+		
+		//print time-stamp of execution to file
 		out.println(LocalDateTime.now().toString() + "\n");
 		out.flush();
+		
+		/* Looping through given list of web elements and grabbing title 
+		 * description and price */
 		for (WebElement item : items) {
-			//System.out.println(item.toString());
-			int index = items.indexOf(item) + 1;
-			String itemPath = "";
 			String line = "";
+			int index = items.indexOf(item) + 1;
+			
+			//getting title description
 			String title = wait.until(ExpectedConditions.visibilityOf(
 					item.findElement(By.cssSelector(
 							"[data-test=product-title]")))).getText();
-			//		By.cssSelector("[data-test = product-title]")).getText();
 			Thread.sleep(1000);
-			String price = item.findElement(By.cssSelector("[data-test=product-card-price]")).getText();//item.findElement(By.className("s-item_price")).getText();
-			System.out.println(price);
+			
+			// getting price
+			String price = item.findElement(By.cssSelector("[data-test=product-card-price]")).getText();
+			
 			line = title + " - " + price;
+			
 			writeToFile(line, out);
-			System.out.println(title);
-			//System.out.println(item.toString());
+			System.out.println(line);
 			if (index == items.size()) {
 				break;
 			}
 		}
 	}
 
+	/** Writes to file specified in class */
 	public static void writeToFile(String text, PrintWriter out) {
 		out.println(text);
 		out.flush();
